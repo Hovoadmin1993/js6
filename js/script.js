ff6bbcf2172467f847f8bbb084df2ea7d12d8d6f
Hovoadmin1993 <hovopetrosyan7@gmail.com>
2021-06-03 18:30:23 +0400

    cv

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
 // tnain
 
-// lini funcianer 
-
-// lega lini 
-
-// mej@ timsss --> objectner
-
-// timi mej xaxacoxner linen 
 
 
-
-const leagues = {
-    leagueA: {
-        teams: [team1, team2, team3 ],
-        bombardir:[]
-    },
-    leagueb:{}
-}
-
-team1 ={
-    players: [],
-    coach: '',
-    assistent: ''
-}
+// createElement ogt. inner html-i poxaren
+// classList.add("")--avelacnel classner@ 
